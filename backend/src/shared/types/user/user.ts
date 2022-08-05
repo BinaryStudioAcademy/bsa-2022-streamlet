0f7f7048ff60e2aa9e05c69c0678105a91198e4f
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2022-08-05 18:58:04 +0300

    backend: fix backend build with shared modules

--- backend/src/shared/types/user/user.ts
+++ backend/src/shared/types/user/user.ts
@@ -1,2 +1,2 @@
-export { type UserSignUpRequestDto } from './user-sign-up-request-dto.type';
-export { type UserSignUpResponseDto } from './user-sign-up-response-dto.type';
+export { type UserSignUpRequestDto } from 'shared/build';
+export { type UserSignUpResponseDto } from 'shared/build';
