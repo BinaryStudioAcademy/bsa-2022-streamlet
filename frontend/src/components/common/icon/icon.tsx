4d5cbf2a1a1ecdd771a240c574bb569e58e8ef33
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-11 19:07:46 +0300

    frontend-43: + icons

--- frontend/src/components/common/icon/icon.tsx
+++ frontend/src/components/common/icon/icon.tsx
@@ -5,9 +5,11 @@ import { ReactComponent as ArrowDown } from '../../../assets/img/arrow-down.svg'
 import { ReactComponent as ArrowUp } from '../../../assets/img/arrow-up.svg';
 import { ReactComponent as Camera } from '../../../assets/img/camera.svg';
 import { ReactComponent as Compas } from '../../../assets/img/compas.svg';
+import { ReactComponent as Filter } from '../../../assets/img/filter.svg';
 import { ReactComponent as Follow } from '../../../assets/img/follow.svg';
 import { ReactComponent as History } from '../../../assets/img/history.svg';
 import { ReactComponent as Home } from '../../../assets/img/home.svg';
+import { ReactComponent as Play } from '../../../assets/img/play.svg';
 import { ReactComponent as Search } from '../../../assets/img/search.svg';
 import { ReactComponent as Signout } from '../../../assets/img/signout.svg';
 import { ReactComponent as Smile } from '../../../assets/img/smile.svg';
@@ -43,6 +45,9 @@ const Icon = ({ name, color, size }: { name: string; color: string; size: string
     case IconName.COMPAS:
       return <Compas className={getColor(color)} width={`${size}px`} height={`${size}px`} />;
 
+    case IconName.FILTER:
+      return <Filter className={getColor(color)} width={`${size}px`} height={`${size}px`} />;
+
     case IconName.FOLLOW:
       return <Follow className={getColor(color)} width={`${size}px`} height={`${size}px`} />;
 
@@ -51,6 +56,9 @@ const Icon = ({ name, color, size }: { name: string; color: string; size: string
 
     case IconName.HOME:
       return <Home className={getColor(color)} width={`${size}px`} height={`${size}px`} />;
+
+    case IconName.PLAY:
+      return <Play className={getColor(color)} width={`${size}px`} height={`${size}px`} />;
 
     case IconName.SEARCH:
       return <Search className={getColor(color)} width={`${size}px`} height={`${size}px`} />;
