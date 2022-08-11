c7ec6a0c424b9e60a8f8d5abd2deaff2d8ca4a34
Vitalii Bedletskyi <vitalii.bedletskyi@binary-studio.com>
2022-08-11 12:00:18 +0300

    fix required className

--- frontend/src/components/common/link/link.tsx
+++ frontend/src/components/common/link/link.tsx
@@ -4,7 +4,7 @@ import { AppRoute } from 'common/enums/enums';
 
 type Props = {
   to: AppRoute;
-  className: string;
+  className?: string;
 };
 
 const Link: FC<Props> = ({ children, to, className }) => (
