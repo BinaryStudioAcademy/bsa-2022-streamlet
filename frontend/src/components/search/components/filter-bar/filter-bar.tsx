99299ea7ce78b763fda15bcc69d399eb76c40f5e
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-12 09:22:42 +0300

    frontend-43: * filter bar component

--- frontend/src/components/search/components/filter-bar/filter-bar.tsx
+++ frontend/src/components/search/components/filter-bar/filter-bar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FC } from 'common/types/types';
 import { IconName } from 'common/enums/components';
 import { Icon } from 'components/common/common';
-import { filterDate, filterType, IFilter } from 'components/search/common/constants';
+import { filterDate, filterType } from 'components/search/common/constants';
 
 import styles from './styles.module.scss';
 
@@ -26,7 +26,7 @@ const FilterBar: FC<Props> = ({ filterType: fType, onChangeFilterType, filterDat
           <label htmlFor="filterBarType" className={styles['filter-bar-select']}>
             <select id="filterBarType" onChange={onHandleChangeFilterType} value={fType}>
               <option value="">all</option>
-              {filterType.map((f: IFilter) => (
+              {filterType.map((f) => (
                 <option key={f.name} value={f.value}>
                   {f.text}
                 </option>
@@ -39,7 +39,7 @@ const FilterBar: FC<Props> = ({ filterType: fType, onChangeFilterType, filterDat
           <label htmlFor="filterBarDate" className={styles['filter-bar-select']}>
             <select id="filterBarDate" onChange={onHandleChangeFilterDate} value={fDate}>
               <option value="">anytime</option>
-              {filterDate.map((f: IFilter) => (
+              {filterDate.map((f) => (
                 <option key={f.name} value={f.value}>
                   {f.text}
                 </option>
