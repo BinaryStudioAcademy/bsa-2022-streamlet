6ded9fff93986d82d6548fe993772faf618df099
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-12 08:38:29 +0300

    frontend-43: + route & hook for search page

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -2,6 +2,7 @@ import { AppRoute } from 'common/enums/enums';
 import { FC } from 'common/types/types';
 import { Routes, Route } from 'components/common/common';
 import { Auth } from 'components/auth/auth';
+import { Search } from 'components/search/search';
 import { NotFound } from '../not-found-page/not-found';
 
 const App: FC = () => {
@@ -11,6 +12,7 @@ const App: FC = () => {
         <Route path={AppRoute.ROOT} element="Root" />
         <Route path={AppRoute.SIGN_UP} element={<Auth />} />
         <Route path={AppRoute.SIGN_IN} element={<Auth />} />
+        <Route path={AppRoute.SEARCH} element={<Search />} />
         <Route path={AppRoute.RESTORE_PASSWORD} element={<Auth />} />
         <Route path={AppRoute.ANY} element={<NotFound />} />
       </Routes>
