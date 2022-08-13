76c69907c3e397b39f9a541d6175da98ca623e99
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-13 06:54:44 +0300

    frontend-43: + div containers to filters

--- frontend/src/components/search/components/common/filter-dropdown/filter-dropdown.tsx
+++ frontend/src/components/search/components/common/filter-dropdown/filter-dropdown.tsx
@@ -18,35 +18,38 @@ type Props = {
 const FilterDropdown: FC<Props> = ({ filter, onChangeFilter, title, options }) => {
   const [toggleDropdown, setToggleDropdown] = useState(false);
 
-  const onHandleToggleDropdown = (): void => setToggleDropdown(!toggleDropdown);
+  const handleToggleDropdown = (): void => setToggleDropdown(!toggleDropdown);
+
   const onHandleChangeFilter = (e: React.FormEvent<HTMLInputElement>): void => onChangeFilter(e.currentTarget.value);
 
   return (
-    <div className={styles['filter-dropdown']}>
-      <div className={styles['filter-dropdown-title']} onClick={onHandleToggleDropdown}>
-        <span>{title}</span>
-        <Icon name={toggleDropdown ? IconName.ARROW_UP : IconName.ARROW_DOWN} />
+    <div>
+      <div className={styles['filter-dropdown']}>
+        <div className={styles['filter-dropdown-title']} onClick={handleToggleDropdown}>
+          <span>{title}</span>
+          <Icon name={toggleDropdown ? IconName.ARROW_UP : IconName.ARROW_DOWN} />
+        </div>
+        {toggleDropdown && (
+          <ul className={styles['filter-dropdown-list']}>
+            {options.map((o) => {
+              const id = `rad-${o.value}`;
+              return (
+                <li key={o.value} className={styles['filter-dropdown-item']}>
+                  <input
+                    id={id}
+                    type="radio"
+                    name={title}
+                    value={o.value}
+                    checked={filter === o.value}
+                    onChange={onHandleChangeFilter}
+                  />
+                  <label htmlFor={id}>{o.text}</label>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
-      {toggleDropdown && (
-        <ul className={styles['filter-dropdown-list']}>
-          {options.map((o) => {
-            const id = `rad-${o.value}`;
-            return (
-              <li key={o.value} className={styles['filter-dropdown-item']}>
-                <input
-                  id={id}
-                  type="radio"
-                  name={title}
-                  value={o.value}
-                  checked={filter === o.value}
-                  onChange={onHandleChangeFilter}
-                />
-                <label htmlFor={id}>{o.text}</label>
-              </li>
-            );
-          })}
-        </ul>
-      )}
     </div>
   );
 };
