960c78c19361f463a8f4ae77e0109b63daeb3371
foxt451 <svpetrenko123@gmail.com>
2022-08-14 11:25:43 +0300

    frontend-25: + redux-persist

--- frontend/src/components/common/loader/loader.tsx
+++ frontend/src/components/common/loader/loader.tsx
@@ -3,14 +3,20 @@ import styles from './styles.module.scss';
 import classNames from 'classnames';
 
 type Props = {
-  centered?: boolean;
+  hCentered?: boolean;
+  vCentered?: boolean;
   className?: string;
   spinnerSize?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 };
 
-const Loader: FC<Props> = ({ centered = true, className, spinnerSize = 'md' }) => {
+const Loader: FC<Props> = ({ hCentered = true, vCentered = true, className, spinnerSize = 'md' }) => {
   return (
-    <div className={classNames(styles['loader-wrapper'], className, { [styles['centered']]: centered })}>
+    <div
+      className={classNames(styles['loader-wrapper'], className, {
+        [styles['h-centered']]: hCentered,
+        [styles['v-centered']]: vCentered,
+      })}
+    >
       <span className={classNames(styles['loader'], styles[spinnerSize])}></span>
     </div>
   );
--- frontend/src/store/root-reducer.ts
+++ frontend/src/store/root-reducer.ts
@@ -1,7 +1,8 @@
+import { combineReducers } from '@reduxjs/toolkit';
 import { reducer as auth } from './auth/reducer';
 
-const rootReducer = {
+const rootReducer = combineReducers({
   auth,
-};
+});
 
 export { rootReducer };
