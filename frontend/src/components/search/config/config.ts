793bce9fe030476364cbcd9638f50151aead5fa5
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-16 08:21:57 +0300

    frontend-43: + search helpers

--- frontend/src/components/search/config/config.ts
+++ frontend/src/components/search/config/config.ts
@@ -2,4 +2,5 @@ export { TypeFilterId, allTypeFilters } from './type-filter.config';
 export { DateFilterId, allDateFilters } from './date-filter.config';
 export { DurationFilterId, allDurationFilters } from './duration-filter.config';
 export { SortByFilterId, allSortByFilters } from './sort-by-filter.config';
-export { SearchQueryParam } from 'store/search/models';
+export { SearchQueryParam, FilterType } from 'store/search/models';
+export { type State as SearchState } from 'store/search/reducer';
