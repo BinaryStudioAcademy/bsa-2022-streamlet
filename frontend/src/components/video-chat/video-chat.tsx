9bfe7b5044bb411ffe88c7bc8a73f306d24937d5
artem-drozdov <owner_drozroff@ukr.net>
2022-08-17 12:23:58 +0300

    frontend-41: + implement video chat component

--- frontend/src/components/video-chat/video-chat.tsx
+++ frontend/src/components/video-chat/video-chat.tsx
@@ -2,47 +2,9 @@ import { FC } from 'common/types/types';
 
 import { SendMessage } from 'components/common/send-message/send-message';
 import { VideoComment } from './video-comment/video-comment';
-import { Comment } from 'shared/src/common/types/comment';
+import { comments } from './comments.mock';
 
 import './video-chat.scss';
-
-const comments: Comment[] = [
-  {
-    id: 1,
-    avatar: 'https://bipbap.ru/wp-content/uploads/2021/07/1551512888_2-730x617.jpg',
-    name: 'Imam Farrhouk',
-    date_added: '4 min ago',
-    text: 'We are facing a serious business dilemma, with Facebook taking away a good chunk of traffic to news and content sites, and ad blockers eating into what’s left of it while slashing ad revenues.',
-  },
-  {
-    id: 2,
-    avatar: 'https://bipbap.ru/wp-content/uploads/2021/07/1551512888_2-730x617.jpg',
-    name: 'Imam Farrhouk',
-    date_added: '4 min ago',
-    text: 'We are facing a serious business dilemma, with Facebook taking away a good chunk of traffic to news.',
-  },
-  {
-    id: 3,
-    avatar: 'https://bipbap.ru/wp-content/uploads/2021/07/1551512888_2-730x617.jpg',
-    name: 'Imam Farrhouk',
-    date_added: '4 min ago',
-    text: 'We are facing a serious business dilemma, and ad blockers eating into what’s left of it while slashing ad revenues.',
-  },
-  {
-    id: 4,
-    avatar: 'https://bipbap.ru/wp-content/uploads/2021/07/1551512888_2-730x617.jpg',
-    name: 'Imam Farrhouk',
-    date_added: '4 min ago',
-    text: 'We are facing a serious business dilemma, with Facebook taking away a good chunk of traffic to news and content sites, and ad blockers eating into what’s left of it while slashing ad revenues.',
-  },
-  {
-    id: 5,
-    avatar: 'https://bipbap.ru/wp-content/uploads/2021/07/1551512888_2-730x617.jpg',
-    name: 'Imam Farrhouk',
-    date_added: '4 min ago',
-    text: 'We are facing a serious business dilemma, with Facebook taking away a good chunk of traffic to news and content sites, and ad blockers eating into what’s left of it while slashing ad revenues.',
-  },
-];
 
 const VideoChat: FC = () => {
   return (
