1fe223780326f7be566394a22e7480db746ef88b
Viktor Sivulskiy <info@softmaker.kz>
2022-08-19 17:41:39 +0600

    Update types.ts

--- backend/core/src/shared/types/types.ts
+++ backend/core/src/shared/types/types.ts
@@ -4,11 +4,9 @@ export * from './amqp/amqp';
 export * from './auth/auth';
 export * from './express';
 export * from './history/history';
-export { MailTestRequestDto } from './mail-test/mail-test';
 export * from './cloudinary/image-store';
 export * from './mail/mail';
 export * from './mail-test/mail-test';
 export * from './rtmp/rtmp';
 export * from './channel/channel';
 export * from './request/request';
-
