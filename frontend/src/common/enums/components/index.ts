7c88bd11b33c559c5386b283287f1d2dabc9c047
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-21 13:10:27 +0300

    frontend-76: + loader enum

--- frontend/src/common/enums/components/index.ts
+++ frontend/src/common/enums/components/index.ts
@@ -1,3 +1,4 @@
 export { IconColor } from './icon-color.enum';
 export { IconName } from './icon-name.enum';
 export { MenuOptions } from './user-menu-options';
+export { LoaderSize } from './loader-size.enum';
