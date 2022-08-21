677dc549d5fbb29a0b0399b946ea2eb9423d5ffe
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-21 13:31:52 +0300

    frontend-78: * video tag component

--- frontend/src/components/search/components/common/video-tag/video-tag.tsx
+++ frontend/src/components/search/components/common/video-tag/video-tag.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'common/types/types';
+import clsx from 'clsx';
+import { VideoTagName } from '../../../config/config';
 import styles from './styles.module.scss';
 
 type Props = {
-  name: string;
+  name: VideoTagName;
 };
 
 const VideoTag: FC<Props> = ({ name }) => (
   <div className={styles.container}>
-    <div className={styles['video-tag']}>
+    <div className={clsx(styles['video-tag'], styles[`video-tag-${name}`])}>
       <span>{name}</span>
     </div>
   </div>
