2432cd15c9cc80bfb3448ad96c9202ea56f2b28c
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-21 08:24:02 +0300

    frontend-86: * app component

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'common/types/types';
 import { AppRoute } from 'common/enums/enums';
-import { useLocation, useAppSelector, useEffect, useAppDispatch } from 'hooks/hooks';
+import { useLocation, useEffect, useAppDispatch } from 'hooks/hooks';
 import { tokensStorageService } from 'services/services';
 import { authActions } from 'store/actions';
 import { MainPage } from 'pages/main-page';
@@ -20,12 +20,8 @@ import styles from './app.module.scss';
 const App: FC = () => {
   const dispatch = useAppDispatch();
   const { pathname } = useLocation();
-  const { user } = useAppSelector((state) => ({
-    user: state.auth.user,
-  }));
 
   const hasToken = Boolean(tokensStorageService.getTokens().accessToken);
-  const hasUser = Boolean(user);
 
   const isHaveHeader = isRouteHaveHeader(pathname);
 
@@ -33,7 +29,7 @@ const App: FC = () => {
     if (hasToken) {
       dispatch(authActions.loadCurrentUser());
     }
-  }, [hasToken, hasUser, dispatch]);
+  }, [hasToken, dispatch]);
 
   return (
     <>
