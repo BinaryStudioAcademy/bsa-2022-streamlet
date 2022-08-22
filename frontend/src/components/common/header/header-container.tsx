3c804a7a3d6f33c3f33bb72cc83c97c15513a212
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-22 06:46:02 +0300

    frontend-78: * header

--- frontend/src/components/common/header/header-container.tsx
+++ frontend/src/components/common/header/header-container.tsx
@@ -1,18 +1,11 @@
+import { MouseEvent, FormEvent } from 'react';
 import { FC } from 'common/types/types';
-import {
-  useOutsideClick,
-  useAppDispatch,
-  useAppSelector,
-  useCallback,
-  useNavigate,
-  useLocation,
-  useId,
-} from 'hooks/hooks';
-import { useState, MouseEvent, FormEvent } from 'react';
-import { Header } from './header';
-import { MenuOptions, IconName, AppRoute, SearchQueryParam } from 'common/enums/enums';
-import { searchActions } from 'store/actions';
+import { MenuOptions, AppRoute, SearchQueryParam } from 'common/enums/enums';
+import { useOutsideClick, useAppDispatch, useAppSelector, useCallback, useNavigate, useRef } from 'hooks/hooks';
+import { authActions, searchActions } from 'store/actions';
 import { NotificationDropdownContainer } from 'components/notification-dropdown/notification-dropdown-container';
+import { allMenuOptions } from './config';
+import { Header } from './header';
 
 const FAKE_USER_AVATAR = 'https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745';
 
@@ -20,45 +13,44 @@ const HeaderContainer: FC = () => {
   const dispatch = useAppDispatch();
   const {
     search: { searchText },
-  } = useAppSelector((state) => ({ search: state.search }));
+    user,
+  } = useAppSelector((state) => ({
+    search: state.search,
+    user: state.auth.user,
+  }));
   const navigate = useNavigate();
-  const { pathname } = useLocation();
 
-  const [isLogged, setIsLogged] = useState(false);
-  const { isOpened: isMenuOpen, close: closeMenu, open: openMenu, ref: menuRef } = useOutsideClick<HTMLDivElement>();
+  const hasUser = Boolean(user);
 
-  const options = [
-    {
-      type: MenuOptions.Settings,
-      text: 'Settings',
-      icon: IconName.SETTINGS,
-      onClick: (): void => {
-        void 1;
-      },
-    },
-    {
-      type: MenuOptions.Theme,
-      text: 'Theme',
-      icon: IconName.MOON,
-      onClick: (): void => {
-        void 1;
-      },
-    },
-    {
-      type: MenuOptions.SignOut,
-      text: 'Sign Out',
-      icon: IconName.SIGN_OUT,
-      onClick: (e: MouseEvent): void => {
-        handleClickSignIn(e);
-      },
-    },
-  ];
+  const { isOpened: isMenuOpen, open: openMenu, ref: menuRef } = useOutsideClick<HTMLDivElement>();
 
-  function handleClickSignIn(e: MouseEvent): void {
-    e.preventDefault();
+  const emptyOnClickHandler = (): void => void 0;
 
-    closeMenu();
-    setIsLogged(!isLogged);
+  const matchMenuOptionWithOnClickHandler: Record<MenuOptions, () => void> = {
+    [MenuOptions.Settings]: emptyOnClickHandler,
+    [MenuOptions.Theme]: emptyOnClickHandler,
+    [MenuOptions.SignOut]: handleClickSignOut,
+  };
+
+  const options = allMenuOptions.map((option) => ({
+    ...option,
+    onClick: matchMenuOptionWithOnClickHandler[option.type],
+  }));
+
+  const handleSignOut = useCallback(async () => {
+    try {
+      await dispatch(authActions.signOut());
+    } finally {
+      navigate(AppRoute.SIGN_IN, { replace: true });
+    }
+  }, [dispatch, navigate]);
+
+  function handleClickSignIn(): void {
+    navigate(AppRoute.SIGN_IN, { replace: true });
+  }
+
+  function handleClickSignOut(): void {
+    handleSignOut();
   }
 
   function handleClickUserMenu(e: MouseEvent): void {
@@ -68,7 +60,7 @@ const HeaderContainer: FC = () => {
     }
   }
 
-  const searchInputId = useId();
+  const searchInputEl = useRef<HTMLInputElement>(null);
 
   const handleClearActiveFilterIds = useCallback(() => dispatch(searchActions.clearActiveFilterIds()), [dispatch]);
 
@@ -80,14 +72,14 @@ const HeaderContainer: FC = () => {
 
   const handleClearInputSearch = (): void => {
     handleInputSearch('');
-    document.getElementById(searchInputId)?.focus();
+    searchInputEl.current?.focus();
   };
 
   const handleClickSearchBtn = (): void => {
     if (searchText) {
       handleClearActiveFilterIds();
       const searchUrlParams = new URLSearchParams({ [SearchQueryParam.SEARCH_TEXT]: searchText });
-      navigate(`/search?${searchUrlParams.toString()}`, { replace: true });
+      navigate(`${AppRoute.SEARCH}?${searchUrlParams.toString()}`, { replace: true });
     }
   };
 
@@ -96,17 +88,13 @@ const HeaderContainer: FC = () => {
     handleClickSearchBtn();
   };
 
-  if (pathname === AppRoute.SIGN_IN || pathname === AppRoute.SIGN_UP || pathname === AppRoute.RESTORE_PASSWORD) {
-    return null;
-  }
-
   return (
     <Header
       menuRef={menuRef}
-      isLogged={isLogged}
+      isLogged={hasUser}
       isMenuOpen={isMenuOpen}
       searchValue={searchText}
-      searchInputId={searchInputId}
+      searchInputEl={searchInputEl}
       handleClickUserMenu={handleClickUserMenu}
       handleClickSignIn={handleClickSignIn}
       handleChangeInputSearch={handleChangeInputSearch}
--- frontend/src/components/common/header/header.tsx
+++ frontend/src/components/common/header/header.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'common/types/types';
-import { Link } from 'react-router-dom';
 import { FormEvent, MouseEvent, RefObject } from 'react';
+import { Link } from 'react-router-dom';
+import { FC } from 'common/types/types';
 import { AppRoute, IconName, MenuOptions } from 'common/enums/enums';
-import { Icon } from '../icon';
 import { useAppSelector, useAppDispatch } from 'hooks/hooks';
 import { closeSidebar, openSidebar } from 'store/layout/actions';
+import { Icon } from 'components/common/common';
 
 import styles from './header.module.scss';
 
@@ -19,9 +19,9 @@ interface HeaderProps {
   isLogged: boolean;
   isMenuOpen: boolean;
   searchValue: string;
-  searchInputId: string;
+  searchInputEl: RefObject<HTMLInputElement>;
   handleClickUserMenu: (e: MouseEvent<HTMLButtonElement>) => void;
-  handleClickSignIn(e: MouseEvent<HTMLElement>): void;
+  handleClickSignIn(): void;
   handleChangeInputSearch(e: FormEvent<HTMLInputElement>): void;
   handleClearInputSearch(e: MouseEvent<HTMLElement>): void;
   handleClickSearchBtn(e: MouseEvent<HTMLElement>): void;
@@ -36,7 +36,7 @@ const Header: FC<HeaderProps> = ({
   isLogged,
   isMenuOpen,
   searchValue,
-  searchInputId,
+  searchInputEl,
   handleClickSignIn,
   handleClickUserMenu,
   handleChangeInputSearch,
@@ -77,7 +77,7 @@ const Header: FC<HeaderProps> = ({
           </div>
           <input
             className={styles['search-input']}
-            id={searchInputId}
+            ref={searchInputEl}
             type="text"
             value={searchValue}
             onChange={handleChangeInputSearch}
