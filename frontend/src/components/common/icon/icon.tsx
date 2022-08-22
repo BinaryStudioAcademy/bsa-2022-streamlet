992cad14433f3499e7aeb5945089a59c145b4c91
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-22 19:35:28 +0300

    frontend-78: + icons

--- frontend/src/components/common/icon/icon.tsx
+++ frontend/src/components/common/icon/icon.tsx
@@ -3,6 +3,8 @@ import { IconName } from 'common/enums/enums';
 import { ReactComponent as Alarm } from 'assets/img/alarm.svg';
 import { ReactComponent as ArrowDown } from 'assets/img/arrow-down.svg';
 import { ReactComponent as ArrowUp } from 'assets/img/arrow-up.svg';
+import { ReactComponent as ArrowLeft } from 'assets/img/arrow-left.svg';
+import { ReactComponent as BellOutline } from 'assets/img/bell-outline.svg';
 import { ReactComponent as Camera } from 'assets/img/camera.svg';
 import { ReactComponent as Compass } from 'assets/img/compass.svg';
 import { ReactComponent as Filter } from 'assets/img/filter.svg';
@@ -30,6 +32,7 @@ import { ReactComponent as Warning } from 'assets/img/warning.svg';
 import { ReactComponent as Success } from 'assets/img/success.svg';
 import { ReactComponent as SendMessage } from 'assets/img/send-message.svg';
 import { ReactComponent as Emoji } from 'assets/img/emoji.svg';
+import { ReactComponent as OnlineStreaming } from 'assets/img/online-streaming.svg';
 
 import clsx from 'clsx';
 
@@ -75,6 +78,10 @@ const Icon = ({ name, color, width, height, className, onClick }: IconProps): JS
 
     case IconName.ARROW_UP: {
       return <ArrowUp {...commonProps} />;
+    }
+
+    case IconName.ARROW_LEFT: {
+      return <ArrowLeft {...commonProps} />;
     }
 
     case IconName.CAMERA: {
@@ -149,17 +156,21 @@ const Icon = ({ name, color, width, height, className, onClick }: IconProps): JS
       return <Close {...commonProps} />;
     }
 
-    case IconName.EXCLAMATION:
+    case IconName.EXCLAMATION: {
       return <Exclamation {...commonProps} />;
+    }
 
-    case IconName.INFO:
+    case IconName.INFO: {
       return <Info {...commonProps} />;
+    }
 
-    case IconName.WARNING:
+    case IconName.WARNING: {
       return <Warning {...commonProps} />;
+    }
 
-    case IconName.SUCCESS:
+    case IconName.SUCCESS: {
       return <Success {...commonProps} />;
+    }
 
     case IconName.SEND_MESSAGE: {
       return <SendMessage {...commonProps} />;
@@ -169,11 +180,21 @@ const Icon = ({ name, color, width, height, className, onClick }: IconProps): JS
       return <Emoji {...commonProps} />;
     }
 
-    case IconName.ANALYTICS:
+    case IconName.ANALYTICS: {
       return <Analytics {...commonProps} />;
+    }
 
-    case IconName.MARK_AS_READ:
+    case IconName.MARK_AS_READ: {
       return <MarkAsRead {...commonProps} />;
+    }
+
+    case IconName.ONLINE_STREAMING: {
+      return <OnlineStreaming {...commonProps} />;
+    }
+
+    case IconName.BELL_OUTLINE: {
+      return <BellOutline {...commonProps} />;
+    }
 
     default: {
       return <TimeAgo {...commonProps} />;
