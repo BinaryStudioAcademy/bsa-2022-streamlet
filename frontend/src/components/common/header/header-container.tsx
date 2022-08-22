3795ea7fad75ef5ab29cefea4d037f37c6bcdd38
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-08-22 20:03:13 +0300

    frontend-78: * redirect replace

--- frontend/src/components/common/header/header-container.tsx
+++ frontend/src/components/common/header/header-container.tsx
@@ -41,12 +41,12 @@ const HeaderContainer: FC = () => {
     try {
       await dispatch(authActions.signOut());
     } finally {
-      navigate(AppRoutes.SIGN_IN, { replace: true });
+      navigate(AppRoutes.ROOT);
     }
   }, [dispatch, navigate]);
 
   function handleClickSignIn(): void {
-    navigate(AppRoutes.SIGN_IN, { replace: true });
+    navigate(AppRoutes.SIGN_IN);
   }
 
   function handleClickSignOut(): void {
@@ -80,7 +80,7 @@ const HeaderContainer: FC = () => {
     if (searchText) {
       handleClearActiveFilterIds();
       const searchUrlParams = new URLSearchParams({ [SearchQueryParam.SEARCH_TEXT]: searchText });
-      navigate(`${AppRoutes.SEARCH}?${searchUrlParams.toString()}`, { replace: true });
+      navigate(`${AppRoutes.SEARCH}?${searchUrlParams.toString()}`);
     }
   };
 
