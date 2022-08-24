2dec8b5c1f840948b8d0ce3d8a9aca5a190e094d
Ihor Pavliuk <nemuzora.nozomu@gmail.com>
2022-08-25 00:54:26 +0300

    frontend-135: * replaced img elements with svgs, base styles

--- frontend/src/components/auth/components/auth-container/auth-container.tsx
+++ frontend/src/components/auth/components/auth-container/auth-container.tsx
@@ -2,8 +2,8 @@ import styles from './styles.module.scss';
 import { FC } from 'common/types/types';
 import React, { ReactNode } from 'react';
 import logo from 'assets/img/logo.svg';
-import zigzag from 'assets/img/auth/auth-zigzag.svg';
-import element from 'assets/img/auth/auth-element.svg';
+import { ReactComponent as Zigzag } from 'assets/img/auth/auth-zigzag.svg';
+import { ReactComponent as Curve1 } from 'assets/img/auth/auth-curve1.svg';
 import clsx from 'clsx';
 
 type Props = {
@@ -16,13 +16,15 @@ type Props = {
 const AuthContainer: FC<Props> = ({ children, pageTitle, className, topLevelErrorComponent }) => {
   return (
     <div className={clsx(styles['auth-background'], className)}>
-      <img src={zigzag} className={styles['background-zigzag']} alt="zigzag" />
-      <img src={zigzag} className={styles['background-zigzag-2']} alt="zigzag" />
-      <img src={element} className={styles['background-element']} alt="element" />
+      <Zigzag className={styles['background-zigzag']} />
+      <Zigzag className={styles['background-zigzag2']} />
+      <Curve1 className={styles['background-curve1']} />
       <div className={styles['auth-modal']}>
         <div className={styles['auth-container']}>
-          <img src={logo} className={styles['auth-logo']} alt="logo" />
-          <h1 className={styles['auth-title']}>{pageTitle}</h1>
+          <div className={styles['auth-header']}>
+            <img src={logo} className={styles['auth-logo']} alt="logo" />
+            <h1 className={styles['auth-title']}>{pageTitle}</h1>
+          </div>
           {topLevelErrorComponent}
           {children}
         </div>
