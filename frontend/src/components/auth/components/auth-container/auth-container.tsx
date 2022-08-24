2aebb53455e835a3e65842c5523ff7d45dc4ecb3
foxt451 <svpetrenko123@gmail.com>
2022-08-25 01:00:00 +0300

    frontend: * minimize forms and make heading in one line

--- frontend/src/components/auth/components/auth-container/auth-container.tsx
+++ frontend/src/components/auth/components/auth-container/auth-container.tsx
@@ -20,8 +20,10 @@ const AuthContainer: FC<Props> = ({ children, pageTitle, className, topLevelErro
       <img src={zigzag} className={styles['background-zigzag-2']} alt="zigzag" />
       <img src={element} className={styles['background-element']} alt="element" />
       <div className={styles['auth-modal']}>
-        <img src={logo} className={styles['auth-logo']} alt="logo" />
-        <h1 className={styles['auth-title']}>{pageTitle}</h1>
+        <div className={styles['heading']}>
+          <img src={logo} className={styles['auth-logo']} alt="logo" />
+          <h1 className={styles['auth-title']}>{pageTitle}</h1>
+        </div>
         {topLevelErrorComponent}
         {children}
       </div>
