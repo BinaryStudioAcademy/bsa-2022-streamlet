e8e86abb9a72e0287cb2dd12ae4299e982af1c92
foxt451 <svpetrenko123@gmail.com>
2022-08-24 13:25:08 +0300

    common-127: * fix bug with auth flow

--- frontend/src/services/http/interceptors/attach-auth-token-interceptor.ts
+++ frontend/src/services/http/interceptors/attach-auth-token-interceptor.ts
@@ -4,7 +4,7 @@ import { PreInterceptor } from './interceptor';
 export const attachAuthTokenInterceptor: PreInterceptor = ({ url, options }) => {
   const accessToken = tokensStorageService.getTokens().accessToken;
   if (accessToken) {
-    (options.headers as Headers).append('Authorization', `Bearer ${accessToken}`);
+    (options.headers as Headers).set('Authorization', `Bearer ${accessToken}`);
   }
   return Promise.resolve([url, options]);
 };
