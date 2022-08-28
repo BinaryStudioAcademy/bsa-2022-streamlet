421b99e070069388dff3d9c1658bfe37cd0bf969
andrii <user12341528@gmail.com>
2022-08-29 00:57:47 +0300

    common-106: * fixed socket connection config

--- frontend/src/common/config/socket.ts
+++ frontend/src/common/config/socket.ts
@@ -1,5 +1,8 @@
+import { ENV } from 'common/enums/enums';
 import { io } from 'socket.io-client';
 
-const socket = io();
+const socket = io(`http://${ENV.PUSH_HOST}:${ENV.PUSH_PORT}`, {
+  transports: ['websocket'],
+});
 
 export { socket };
--- frontend/src/common/enums/app/env.enum.ts
+++ frontend/src/common/enums/app/env.enum.ts
@@ -1,7 +1,9 @@
-const { REACT_APP_API_ORIGIN_URL } = process.env;
+const { REACT_APP_API_ORIGIN_URL, PUSH_HOST, PUSH_PORT } = process.env;
 
 const ENV = {
   API_PATH: REACT_APP_API_ORIGIN_URL ?? '',
+  PUSH_HOST: PUSH_HOST || 'localhost',
+  PUSH_PORT: Number(PUSH_PORT) || 5002,
 } as const;
 
 export { ENV };
