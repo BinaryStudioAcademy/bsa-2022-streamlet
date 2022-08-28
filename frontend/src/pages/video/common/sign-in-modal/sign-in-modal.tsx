564215cefa54b1de1fc4d7ec217031798cef3dc8
SerGo3580 <SerGo3577@gmail.com>
2022-08-28 20:05:32 +0300

    frontend-42: + modal when user not sign in and try react video

---
+++ frontend/src/pages/video/common/sign-in-modal/sign-in-modal.tsx
@@ -0,0 +1,60 @@
+import { FC } from 'common/types/types';
+import styles from './styles.module.scss';
+import React, { useEffect, RefObject } from 'react';
+import { useRef } from '../../../../hooks/hooks';
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../../../common/enums/app/app';
+type Event = MouseEvent | TouchEvent;
+
+const useOnClickOutside = <T extends HTMLElement = HTMLElement>(
+  ref: RefObject<T>,
+  handler: (event: Event) => void,
+): void => {
+  useEffect(() => {
+    const listener = (event: Event): void => {
+      const el = ref?.current;
+      if (!el || el.contains((event?.target as Node) || null)) {
+        return;
+      }
+
+      handler(event); // Call the handler only if the click is outside of the element passed.
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return (): void => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, handler]); // Reload only if ref or handler changes
+};
+
+type Props = {
+  headerText: string;
+  mainText: string;
+  onClose: { (): void };
+};
+
+const NeedSignInModal: FC<Props> = ({ headerText, mainText, onClose }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+  const clickOutsideHandle = (): void => {
+    onClose();
+  };
+  useOnClickOutside(modalRef, clickOutsideHandle);
+  return (
+    <>
+      <div className={styles['need-sign-in-modal']} ref={modalRef}>
+        <div className={styles['need-sign-in-modal-header']}>{headerText}</div>
+        <div className={styles['need-sign-in-main-text']}>{mainText}</div>
+        <div className={styles['redirect-to-sign-in']}>
+          <Link to={AppRoutes.SIGN_IN} className={styles['redirect-to-sign-in-link']}>
+            Sign in
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { NeedSignInModal };
