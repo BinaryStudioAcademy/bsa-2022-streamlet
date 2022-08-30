6f5200270bf02b1d09970a90a55fab9b029f7f0e
andrii <user12341528@gmail.com>
2022-08-30 22:40:06 +0300

    frontend-183: + basic markup for channel settings page

--- frontend/src/pages/studio/channel/channel.tsx
+++ frontend/src/pages/studio/channel/channel.tsx
@@ -1,12 +1,55 @@
 import { FC } from 'common/types/types';
+import { useState } from 'react';
+import { ChannelProfileUpdateRequestDto } from 'shared/build';
+import { ChannelSettingsForm } from './common/channel-settings-form';
 
 import styles from './styles.module.scss';
 
 const StudioChannel: FC = () => {
+  const [formError] = useState<string | undefined>();
+  const [formLoading] = useState<boolean>(false);
+
+  const onFormSubmit = async (submitValue: ChannelProfileUpdateRequestDto): Promise<void> => {
+    /* eslint no-console: 0 */
+    console.warn(submitValue);
+  };
   return (
-    <div className={styles['studio']}>
-      <h1 className={styles['header']}>Your channel</h1>
-      <h1 className={styles['body']}></h1>
+    <div className={styles['preference-page-content-container']}>
+      <div className={styles['channel-preferences-container']}>
+        <div className={styles['channel-preferences-main-content-container']}>
+          <div className={styles['image-setting-container']}>
+            <h2 className={styles['channel-image-header']}>channel avatar</h2>
+            <div className={styles['channel-image-upload-container']}>
+              <div className={styles['image-upload-control-container']}>
+                <button type="button" className={styles['image-upload-button']}>
+                  Upload channel avatar
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className={styles['image-setting-container']}>
+            <h2 className={styles['channel-image-header']}>channel banner</h2>
+            <div className={styles['channel-image-upload-container']}>
+              <div className={styles['image-upload-control-container']}>
+                <button type="button" className={styles['image-upload-button']}>
+                  Upload channel banner
+                </button>
+              </div>
+            </div>
+          </div>
+          <h2 className={styles['channel-settings-header']}>Channel preferences</h2>
+          <h3 className={styles['channel-settings-sub-header']}>Change information about your channel</h3>
+          <ChannelSettingsForm
+            onSubmit={onFormSubmit}
+            error={formError}
+            isLoading={formLoading}
+            defaultFormValue={{
+              name: 'Channel name',
+              description: 'Channel description',
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ frontend/src/pages/studio/channel/common/channel-settings-form.tsx
@@ -0,0 +1,56 @@
+import { useAppForm } from 'hooks/hooks';
+import { FC } from 'react';
+import styles from './styles.module.scss';
+import { ChannelProfileUpdateRequestDto, channelUpdateValidationSchema } from 'shared/build';
+import { Input, Loader } from 'components/common/common';
+
+type Props = {
+  onSubmit: { (submitValue: ChannelProfileUpdateRequestDto): Promise<void> };
+  isLoading: boolean;
+  defaultFormValue: ChannelProfileUpdateRequestDto;
+  error: string | undefined;
+};
+
+export const ChannelSettingsForm: FC<Props> = ({ onSubmit, isLoading, defaultFormValue, error }) => {
+  const { errors, control, handleSubmit } = useAppForm<ChannelProfileUpdateRequestDto>({
+    defaultValues: defaultFormValue,
+    validationSchema: channelUpdateValidationSchema,
+    mode: 'onChange',
+  });
+  return (
+    <form className={styles['channel-settings-block']} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles['channel-setting-change-block']}>
+        <div className={styles['channel-setting-input-block']}>
+          <Input
+            control={control}
+            errors={errors}
+            name="name"
+            type="text"
+            inputClassName={styles['input']}
+            label="Channel name"
+            placeholder="Enter new channel name..."
+          />
+        </div>
+      </div>
+      <div className={styles['channel-setting-change-block']}>
+        <div className={styles['channel-setting-input-block']}>
+          <Input
+            control={control}
+            errors={errors}
+            name="description"
+            type="text"
+            inputClassName={styles['input']}
+            label="Description"
+            placeholder="Enter new channel description..."
+          />
+        </div>
+      </div>
+      <div className={styles['save-change-button-container']}>
+        {error && <span className={styles['error-message']}>{error}</span>}
+        <button type="submit" className={styles['save-change-button']}>
+          {isLoading ? <Loader hCentered={true} vCentered={true} spinnerSize="16px" color="white" /> : 'Save changes'}
+        </button>
+      </div>
+    </form>
+  );
+};
