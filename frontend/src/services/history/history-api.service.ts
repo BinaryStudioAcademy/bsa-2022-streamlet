e87c439e7af552c92db85e6f481921bea25de238
serhii <sergo3577@gmail.com>
2022-08-31 18:35:22 +0300

    frontned-107: * remove unused service functionality

--- frontend/src/services/history/history-api.service.ts
+++ frontend/src/services/history/history-api.service.ts
@@ -1,6 +1,6 @@
-import { ApiPath, ContentType, HttpMethod, HistoryApiPath } from 'common/enums/enums';
+import { ApiPath, HttpMethod, HistoryApiPath } from 'common/enums/enums';
 import { Http } from '../http/http.service';
-import { HistoryRequestDto, HistoryResponseDto } from '../../common/types/history/history';
+import { HistoryResponseDto } from '../../common/types/history/history';
 
 type Constructor = {
   http: Http;
@@ -16,16 +16,6 @@ class HistoryApi {
     this.#apiPrefix = apiPrefix;
   }
 
-  public addHVideoHistoryRecord(payload: HistoryRequestDto): Promise<HistoryResponseDto> {
-    return this.#http.load({
-      url: `${this.#apiPrefix}${ApiPath.HISTORY}${HistoryApiPath.ROOT}`,
-      options: {
-        method: HttpMethod.POST,
-        contentType: ContentType.JSON,
-        payload: JSON.stringify(payload),
-      },
-    });
-  }
   public getUserHVideoHistoryRecord(page: number): Promise<HistoryResponseDto> {
     return this.#http.load({
       url: `${this.#apiPrefix}${ApiPath.HISTORY}${HistoryApiPath.ROOT}${page}`,
