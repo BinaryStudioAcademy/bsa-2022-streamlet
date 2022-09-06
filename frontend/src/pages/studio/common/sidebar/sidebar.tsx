f54bc35bd863127327ca2a1bb0e664826ad7e6e3
serhii_hnidobor <sergo3577@gmail.com>
2022-09-06 18:47:44 +0300

    frontend-64: + tooltrips for side bar in studio app

--- frontend/src/pages/studio/common/sidebar/sidebar.tsx
+++ frontend/src/pages/studio/common/sidebar/sidebar.tsx
@@ -7,8 +7,11 @@ import { NavLink } from 'react-router-dom';
 import { ReactNode } from 'react';
 
 import styles from './styles.module.scss';
+import ReactTooltip from 'react-tooltip';
+import { useAppSelector } from '../../../../hooks/hooks';
 
 const StudioSidebar: FC = () => {
+  const isLightTheme = useAppSelector((state) => state.theme.isLightTheme);
   return (
     <aside className={styles['sidebar']}>
       <NavLink to={AppRoutes.ROOT}>
@@ -16,12 +19,17 @@ const StudioSidebar: FC = () => {
           <Icon name={IconName.MAIN_LOGO} width="24" height="24" />
         </div>
       </NavLink>
-
+      <ReactTooltip
+        place="right"
+        backgroundColor={isLightTheme ? '#c3cfc0' : '#000000'}
+        effect="solid"
+        textColor={isLightTheme ? '#000000' : '#ffffff'}
+      />
       {sideBarItems.map((item: ISideBarItem) => (
         <NavLink key={item.itemName} to={item.routeName as AppRoute}>
           {({ isActive }): ReactNode => {
             return (
-              <div className={cn(styles['button'], isActive && styles['active'])}>
+              <div className={cn(styles['button'], isActive && styles['active'])} data-tip={item.itemName}>
                 <Icon name={item.itemName} width="24" height="24" />
               </div>
             );
