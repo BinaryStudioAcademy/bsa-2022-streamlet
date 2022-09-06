f041a6336c883a58920229290330bba6becf45cb
serhii_hnidobor <sergo3577@gmail.com>
2022-09-07 00:13:50 +0300

    frontend-64: tooltip config ussage fix

--- frontend/src/components/common/tooltip/tooltip.tsx
+++ frontend/src/components/common/tooltip/tooltip.tsx
@@ -1,6 +1,12 @@
 import { ReactElement } from 'react';
 import { FC } from '../../../common/types/react/fc.type';
 import ReactTooltip from 'react-tooltip';
+import {
+  DARK_THEME_TOOLTIP_BACKGROUND_COLOR,
+  DARK_THEME_TOOLTIP_TEXT_COLOR,
+  LIGHT_THEME_TOOLTIP_BACKGROUND_COLOR,
+  LIGHT_THEME_TOOLTIP_TEXT_COLOR,
+} from './tooltip.config';
 
 type props = {
   place: 'left' | 'right' | 'top' | 'bottom';
@@ -9,8 +15,8 @@ type props = {
 
 const Tooltip: FC<props> = ({ isLightTheme, place }): ReactElement => {
   const colorForTooltip = {
-    backgroundColor: isLightTheme ? '#c3cfc0' : '#000000',
-    textColor: isLightTheme ? '#000000' : '#ffffff',
+    backgroundColor: isLightTheme ? LIGHT_THEME_TOOLTIP_BACKGROUND_COLOR : DARK_THEME_TOOLTIP_BACKGROUND_COLOR,
+    textColor: isLightTheme ? LIGHT_THEME_TOOLTIP_TEXT_COLOR : DARK_THEME_TOOLTIP_TEXT_COLOR,
   };
 
   return (
