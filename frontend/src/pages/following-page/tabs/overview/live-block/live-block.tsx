2b50c62c74f9c62582b69c7399d246fe5940aa5c
foxt451 <svpetrenko123@gmail.com>
2022-09-06 13:51:34 +0300

    frontend: * redesign page

--- frontend/src/pages/following-page/tabs/overview/live-block/live-block.tsx
+++ frontend/src/pages/following-page/tabs/overview/live-block/live-block.tsx
@@ -1,5 +1,5 @@
 import { DataStatus, ErrorMessage, LoaderSize } from 'common/enums/enums';
-import { Button, Loader, VideoCardMain } from 'components/common/common';
+import { Loader, VideoCardMain } from 'components/common/common';
 import { ErrorBox } from 'components/common/errors/errors';
 import { useAppDispatch, useAppSelector, useNavigate } from 'hooks/hooks';
 import React, { FC, useEffect } from 'react';
@@ -7,10 +7,10 @@ import { shallowEqual } from 'react-redux';
 import { useOutletContext } from 'react-router-dom';
 import { loadLiveVideos } from 'store/following-page/actions';
 import { Tab } from '../../tab';
-import { BlockContainer } from '../block-container/block-container';
 import { MAX_VIDEOS_IN_BLOCK, SHOW_SEE_ALL_VIDEOS_AFTER } from '../constants';
-import { ScrollableVideosList } from '../scrollable-videos-list/scrollable-videos-list';
+import { VideosList } from '../videos-list/videos-list';
 import styles from '../styles.module.scss';
+import { SeeMoreCard } from '../see-more-card/see-more-card';
 
 const LiveBlock: FC = () => {
   const navigate = useNavigate();
@@ -38,28 +38,33 @@ const LiveBlock: FC = () => {
   if (videos.length === 0 && dataStatus === DataStatus.FULFILLED) return null;
 
   return (
-    <BlockContainer title="New live streams">
+    <>
       {dataStatus === DataStatus.REJECTED && <ErrorBox message={error || ErrorMessage.DEFAULT} />}
       {dataStatus === DataStatus.PENDING ? (
         <Loader hCentered vCentered spinnerSize={LoaderSize.SM} className={styles['spinner']} />
       ) : (
-        <ScrollableVideosList>
-          {videos.slice(0, MAX_VIDEOS_IN_BLOCK).map((video) => {
-            return <VideoCardMain video={video} isLightTheme={isLightTheme} key={video.id} />;
-          })}
-        </ScrollableVideosList>
+        <VideosList title="New live streams">
+          {videos
+            .slice(0, MAX_VIDEOS_IN_BLOCK)
+            .map((video) => {
+              return <VideoCardMain video={video} isLightTheme={isLightTheme} key={video.id} />;
+            })
+            .concat(
+              videos.length >= SHOW_SEE_ALL_VIDEOS_AFTER ? (
+                <SeeMoreCard
+                  text="Show All"
+                  onBtnClick={(): void => {
+                    pageContainerRef.current?.scrollTo(0, 0);
+                    navigate(`../${Tab.LIVE}`);
+                  }}
+                />
+              ) : (
+                []
+              ),
+            )}
+        </VideosList>
       )}
-      {videos.length >= SHOW_SEE_ALL_VIDEOS_AFTER && (
-        <Button
-          content="See all live"
-          className={styles['see-all-btn']}
-          onClick={(): void => {
-            pageContainerRef.current?.scrollTo(0, 0);
-            navigate(`../${Tab.LIVE}`);
-          }}
-        />
-      )}
-    </BlockContainer>
+    </>
   );
 };
 
