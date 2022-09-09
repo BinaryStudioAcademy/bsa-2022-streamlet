f9b608d8e178a1b0222ce7aabba61cb2578faa39
serhii_hnidobor <sergo3577@gmail.com>
2022-09-09 23:02:06 +0300

    frontend-261: + validation channel name and description

--- shared/src/common/enums/channel/channel-profile-change-validation-message.ts
+++ shared/src/common/enums/channel/channel-profile-change-validation-message.ts
@@ -4,4 +4,6 @@ export const ChannelProfileValidationMessage = {
   CHANNEL_NAME_MIN: 'Channel name must contain at least 4 characters',
   CHANNEL_NAME_MAX: 'Channel name must contain no more than 16 characters',
   DESCRIPTION_MIN: 'Channel description must contain at least 10 characters',
+  CHANNEL_NAME_WRONG_REGEXP: 'Only latin characters and some special characters are allowed',
+  CHANNEL_DESCRIPTION_WRONG_REGEXP: 'Cyrillic is not allowed',
 };
