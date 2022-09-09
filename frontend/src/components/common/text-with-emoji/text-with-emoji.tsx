2811b6b8aeced5ee938d87d2a234ef546ed1bc5d
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-09-09 13:19:15 +0300

    frontend-110: * fix text-with-emoji

--- frontend/src/components/common/text-with-emoji/text-with-emoji.tsx
+++ frontend/src/components/common/text-with-emoji/text-with-emoji.tsx
@@ -19,7 +19,7 @@ const TextWithEmoji: FC<Props> = ({ text, emojiStyle, emojiClassname, textClassN
   const emojiReg = /\p{Emoji}(\u200d\p{Emoji})*/gu;
 
   const textParts = text.split(emojiReg);
-  const emojiParts = text.match(emojiReg)?.map((e) => e.codePointAt(0)?.toString(16));
+  const emojiParts = text.match(emojiReg)?.map((e) => (e.length === 1 ? e : e.codePointAt(0)?.toString(16)));
 
   let index = 0;
 
@@ -27,6 +27,9 @@ const TextWithEmoji: FC<Props> = ({ text, emojiStyle, emojiClassname, textClassN
     <p className={clsx(styles['text-with-emoji'], textClassName)}>
       {textParts.map((tp, i) => {
         if (typeof tp === 'undefined' && emojiParts && emojiParts[index]) {
+          if (emojiParts[index]?.length === 1) {
+            return <span key={`${textId}-n-${index}`}>{emojiParts[index++]}</span>;
+          }
           return (
             <img
               key={`${textId}-e-${index}`}
