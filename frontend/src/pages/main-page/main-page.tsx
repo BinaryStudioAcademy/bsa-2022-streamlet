8c4e71e609e6baa382dcdba2354c544b2b7af5f1
artem-drozdov <owner_drozroff@ukr.net>
2022-09-09 15:19:14 +0300

    fixed number of displaying videos on main page

--- frontend/src/pages/main-page/main-page.tsx
+++ frontend/src/pages/main-page/main-page.tsx
@@ -3,7 +3,9 @@ import { FilterBlockProps, FiltersBlock } from 'components/common/filters-block'
 import { PreferencesModalContainer } from 'components/common/preferences-modal/preferences-modal-container';
 import { EMPTY_VIDEO_BLOCK } from 'components/common/video-skeleton/video-skeleton.config';
 import { VideoBlockProps, VideosBlock } from 'components/common/videos-block/videos-block';
-import { useAppSelector } from 'hooks/hooks';
+import { useAppDispatch, useAppSelector, useEffect, useWindowDimensions } from 'hooks/hooks';
+import { setNumberOfVideoForLoading } from 'store/videos/actions';
+import { matchScreenSize, ScreenSizesForMainPage } from './main-page-number-of-videos-loading.config';
 
 import styles from './main-page.module.scss';
 
@@ -13,7 +15,29 @@ interface MainPageProps {
 }
 
 const MainPage: FC<MainPageProps> = ({ filterBlockProps, blocksVideo }) => {
+  const dispatch = useAppDispatch();
   const loadingStatus = useAppSelector((state) => state.videos.dataStatus);
+  const { width } = useWindowDimensions();
+
+  useEffect(calculateNumberOfVideosLoading, [width, dispatch]);
+
+  function calculateNumberOfVideosLoading(): void {
+    if (width < ScreenSizesForMainPage.SMALL) {
+      dispatch(setNumberOfVideoForLoading({ numberOfItems: matchScreenSize[ScreenSizesForMainPage.SMALL] }));
+      return;
+    }
+
+    if (width >= ScreenSizesForMainPage.SMALL && width < ScreenSizesForMainPage.AVERAGE) {
+      dispatch(setNumberOfVideoForLoading({ numberOfItems: matchScreenSize[ScreenSizesForMainPage.AVERAGE] }));
+      return;
+    }
+
+    if (width >= ScreenSizesForMainPage.AVERAGE && width < ScreenSizesForMainPage.LARGE) {
+      dispatch(setNumberOfVideoForLoading({ numberOfItems: matchScreenSize[ScreenSizesForMainPage.LARGE] }));
+      return;
+    }
+    dispatch(setNumberOfVideoForLoading({ numberOfItems: matchScreenSize[ScreenSizesForMainPage.VERY_LARGE] }));
+  }
 
   return (
     <main className={styles.main}>
