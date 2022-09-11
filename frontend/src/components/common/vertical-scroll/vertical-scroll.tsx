fce970dc302c9efb5d5e93aa697683f46d6cc003
serhii_hnidobor <sergo3577@gmail.com>
2022-09-11 19:36:03 +0300

    frontend-173: * fix pr remark & tag scroll edge

--- frontend/src/components/common/vertical-scroll/vertical-scroll.tsx
+++ frontend/src/components/common/vertical-scroll/vertical-scroll.tsx
@@ -1,54 +1,67 @@
-import React, { ReactElement } from 'react';
 import styles from './style.module.scss';
 import { Button } from '../button/button';
 import { VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { useEffect, useState } from '../../../hooks/hooks';
+import { ReactElement, ReactNode, useContext } from 'react';
+import clsx from 'clsx';
 
 const Arrow = ({
   children,
   disabled,
   onClick,
+  isRight,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
   disabled: boolean;
   onClick: VoidFunction;
+  isRight: boolean;
 }): ReactElement => {
+  if (isRight) {
+    return (
+      <div className={styles['arrow-container']}>
+        {!disabled && <div className={styles['blur-container']} />}
+        <Button disabled={disabled} onClick={onClick} content={children} className={styles['arrow']} />
+      </div>
+    );
+  }
+
   return (
     <div className={styles['arrow-container']}>
+      {!disabled && <div className={clsx(styles['blur-container'], styles['blur-container-left'])} />}
       <Button disabled={disabled} onClick={onClick} content={children} className={styles['arrow']} />
     </div>
   );
 };
 
 const LeftArrow = (): ReactElement => {
-  const { isFirstItemVisible, scrollPrev, visibleItemsWithoutSeparators, initComplete } =
-    React.useContext(VisibilityContext);
+  const { isFirstItemVisible, scrollPrev, visibleItemsWithoutSeparators, initComplete } = useContext(VisibilityContext);
 
-  const [disabled, setDisabled] = React.useState(!initComplete || (initComplete && isFirstItemVisible));
-  React.useEffect(() => {
+  const [disabled, setDisabled] = useState(!initComplete || (initComplete && isFirstItemVisible));
+  useEffect(() => {
     if (visibleItemsWithoutSeparators.length) {
       setDisabled(isFirstItemVisible);
     }
   }, [isFirstItemVisible, visibleItemsWithoutSeparators]);
 
   return (
-    <Arrow disabled={disabled} onClick={(): void => scrollPrev() as void}>
+    <Arrow disabled={disabled} onClick={(): void => scrollPrev() as void} isRight={false}>
       {'<'}
     </Arrow>
   );
 };
 
 const RightArrow = (): ReactElement => {
-  const { isLastItemVisible, scrollNext, visibleItemsWithoutSeparators } = React.useContext(VisibilityContext);
+  const { isLastItemVisible, scrollNext, visibleItemsWithoutSeparators } = useContext(VisibilityContext);
 
-  const [disabled, setDisabled] = React.useState(!visibleItemsWithoutSeparators.length && isLastItemVisible);
-  React.useEffect(() => {
+  const [disabled, setDisabled] = useState(!visibleItemsWithoutSeparators.length && isLastItemVisible);
+  useEffect(() => {
     if (visibleItemsWithoutSeparators.length) {
       setDisabled(isLastItemVisible);
     }
   }, [isLastItemVisible, visibleItemsWithoutSeparators]);
 
   return (
-    <Arrow disabled={disabled} onClick={(): void => scrollNext() as void}>
+    <Arrow disabled={disabled} onClick={(): void => scrollNext() as void} isRight={true}>
       {'>'}
     </Arrow>
   );
