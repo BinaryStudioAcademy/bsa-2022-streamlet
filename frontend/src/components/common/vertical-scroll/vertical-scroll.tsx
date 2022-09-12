f47654d22fd4202b404f2d921d4495870676c626
serhii_hnidobor <sergo3577@gmail.com>
2022-09-12 18:07:17 +0300

    frontend-282: + horizontal scroll on following page

--- frontend/src/components/common/vertical-scroll/vertical-scroll.tsx
+++ frontend/src/components/common/vertical-scroll/vertical-scroll.tsx
@@ -4,22 +4,35 @@ import { VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { useEffect, useState } from '../../../hooks/hooks';
 import { ReactElement, ReactNode, useContext } from 'react';
 import clsx from 'clsx';
+import { FC } from '../../../common/types/react/fc.type';
+
+type PropsLeftRightArrow = {
+  isFollowingOrBrowse?: boolean;
+};
 
 const Arrow = ({
   children,
   disabled,
   onClick,
   isRight,
+  isFollowingOrBrowse = false,
 }: {
   children: ReactNode;
   disabled: boolean;
   onClick: VoidFunction;
   isRight: boolean;
+  isFollowingOrBrowse?: boolean;
 }): ReactElement => {
   if (isRight) {
     return (
       <div className={styles['arrow-container']}>
-        {!disabled && <div className={styles['blur-container']} />}
+        {!disabled && (
+          <div
+            className={clsx(styles['blur-container'], {
+              [styles['blur-container-following-browse']]: isFollowingOrBrowse && isRight,
+            })}
+          />
+        )}
         <Button disabled={disabled} onClick={onClick} content={children} className={styles['arrow']} />
       </div>
     );
@@ -27,13 +40,19 @@ const Arrow = ({
 
   return (
     <div className={styles['arrow-container']}>
-      {!disabled && <div className={clsx(styles['blur-container'], styles['blur-container-left'])} />}
+      {!disabled && (
+        <div
+          className={clsx(styles['blur-container'], [styles['blur-container-left']], {
+            [styles['blur-container-following-browse-left']]: isFollowingOrBrowse,
+          })}
+        />
+      )}
       <Button disabled={disabled} onClick={onClick} content={children} className={styles['arrow']} />
     </div>
   );
 };
 
-const LeftArrow = (): ReactElement => {
+const LeftArrow: FC<PropsLeftRightArrow> = ({ isFollowingOrBrowse = false }): ReactElement => {
   const { isFirstItemVisible, scrollPrev, visibleItemsWithoutSeparators, initComplete } = useContext(VisibilityContext);
 
   const [disabled, setDisabled] = useState(!initComplete || (initComplete && isFirstItemVisible));
@@ -44,13 +63,18 @@ const LeftArrow = (): ReactElement => {
   }, [isFirstItemVisible, visibleItemsWithoutSeparators]);
 
   return (
-    <Arrow disabled={disabled} onClick={(): void => scrollPrev() as void} isRight={false}>
+    <Arrow
+      disabled={disabled}
+      onClick={(): void => scrollPrev() as void}
+      isRight={false}
+      isFollowingOrBrowse={isFollowingOrBrowse}
+    >
       {'<'}
     </Arrow>
   );
 };
 
-const RightArrow = (): ReactElement => {
+const RightArrow: FC<PropsLeftRightArrow> = ({ isFollowingOrBrowse = false }): ReactElement => {
   const { isLastItemVisible, scrollNext, visibleItemsWithoutSeparators } = useContext(VisibilityContext);
 
   const [disabled, setDisabled] = useState(!visibleItemsWithoutSeparators.length && isLastItemVisible);
@@ -61,7 +85,12 @@ const RightArrow = (): ReactElement => {
   }, [isLastItemVisible, visibleItemsWithoutSeparators]);
 
   return (
-    <Arrow disabled={disabled} onClick={(): void => scrollNext() as void} isRight={true}>
+    <Arrow
+      disabled={disabled}
+      onClick={(): void => scrollNext() as void}
+      isRight={true}
+      isFollowingOrBrowse={isFollowingOrBrowse}
+    >
       {'>'}
     </Arrow>
   );
