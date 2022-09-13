9d5b74e93fc6c9e6f3129fa2401955c6df26ecf1
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-09-13 10:20:44 +0300

    backend-265: + video stats schema

---
+++ backend/core/prisma/migrations/20220914071907_add_video_statistic_model/migration.sql
@@ -0,0 +1,34 @@
+-- CreateEnum
+CREATE TYPE "DeviceCategory" AS ENUM ('MOBILE', 'TABLET', 'DESKTOP', 'UNKNOWN');
+
+-- CreateEnum
+CREATE TYPE "ReactionStatus" AS ENUM ('NONE', 'LIKED', 'DISLIKED');
+
+-- CreateEnum
+CREATE TYPE "SubscriptionStatus" AS ENUM ('NONE', 'SUBSCRIBED', 'UNSUBSCRIBED');
+
+-- CreateTable
+CREATE TABLE "VideoStats" (
+    "id" TEXT NOT NULL,
+    "videoId" TEXT NOT NULL,
+    "userId" TEXT,
+    "watchTime" INTEGER NOT NULL,
+    "device" "DeviceCategory" NOT NULL DEFAULT 'UNKNOWN',
+    "language" TEXT NOT NULL,
+    "isLive" BOOLEAN NOT NULL,
+    "reaction" "ReactionStatus" NOT NULL DEFAULT 'NONE',
+    "subscription" "SubscriptionStatus" NOT NULL DEFAULT 'NONE',
+    "wasSubscribed" BOOLEAN NOT NULL DEFAULT false,
+    "commentsActivity" INTEGER NOT NULL DEFAULT 0,
+    "chatsActivity" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "VideoStats_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "VideoStats" ADD CONSTRAINT "VideoStats_videoId_fkey" FOREIGN KEY ("videoId") REFERENCES "Video"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "VideoStats" ADD CONSTRAINT "VideoStats_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
