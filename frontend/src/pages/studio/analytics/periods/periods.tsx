51d0fd256ee7b91615ff3fabecfc081008f7131d
Viktor Sivulskiy <info@softmaker.kz>
2022-09-14 13:51:09 +0600

    task/frontend-264 + add  studio analytics page layout

--- frontend/src/pages/studio/analytics/periods/periods.tsx
+++ frontend/src/pages/studio/analytics/periods/periods.tsx
@@ -33,38 +33,3 @@ export const Periods: FC<Props> = ({ onChange }) => {
     </div>
   );
 };
-
-{
-  /* <details class="custom-select">
-  <summary class="radios">
-    <input type="radio" name="item" id="default" title="Auswählen..." checked>
-    <input type="radio" name="item" id="item1" title="Item 1">
-    <input type="radio" name="item" id="item2" title="Item 2">
-    <input type="radio" name="item" id="item3" title="Item 3">
-    <input type="radio" name="item" id="item4" title="Item 4">
-    <input type="radio" name="item" id="item5" title="Item 5">
-  </summary>
-  <ul class="list">
-    <li>
-      <label for="item1">
-        Item 1
-        <span></span>
-      </label>
-    </li>
-    <li>
-      <label for="item2">Item 2</label>
-    </li>
-    <li>
-      <label for="item3">Item 3</label>
-    </li>
-    <li>
-      <label for="item4">Item 4</label>
-    </li>
-    <li>
-      <label for="item5">Item 5</label>
-    </li>
-  </ul>
-</details>
-https://www.sliderrevolution.com/resources/css-select-styles/
-*/
-}
