b557e2fa247fd6f910e96eedb8c17c0ad9c05f17
Viktor Sivulskiy <info@softmaker.kz>
2022-09-19 19:09:54 +0600

    fix/frontend-312 * implement modal for streaming

--- frontend/src/components/common/protected-route/protected-route.tsx
+++ frontend/src/components/common/protected-route/protected-route.tsx
@@ -1,8 +1,9 @@
 import { Navigate } from 'react-router-dom';
-import { useAppDispatch, useAppSelector, useEffect } from 'hooks/hooks';
+import { useAppDispatch, useAppSelector, useEffect, useLocation, useNavigate } from 'hooks/hooks';
 import { AppRoutes } from 'common/enums/enums';
 import { FC } from 'common/types/types';
 import { getUserStreamPermission } from 'store/auth/actions';
+import { StreamPermission } from 'shared/build';
 
 type Props = {
   element: JSX.Element;
@@ -10,16 +11,22 @@ type Props = {
 
 const ProtectedRoute: FC<Props> = ({ element }) => {
   const dispatch = useAppDispatch();
-  const { user } = useAppSelector((state) => ({
+  const navigate = useNavigate();
+  const { user, streamPermission } = useAppSelector((state) => ({
     user: state.auth.user,
     streamPermission: state.auth.streamPermission,
   }));
+  const { pathname } = useLocation();
 
   useEffect(() => {
     dispatch(getUserStreamPermission());
   }, [dispatch]);
 
   const hasUser = Boolean(user);
+
+  if (pathname.includes('/studio') && streamPermission.streamPermission !== StreamPermission.ALLOWED) {
+    navigate(AppRoutes.ROOT, { replace: true });
+  }
 
   return hasUser ? element : <Navigate to={{ pathname: AppRoutes.SIGN_IN }} />;
 };
