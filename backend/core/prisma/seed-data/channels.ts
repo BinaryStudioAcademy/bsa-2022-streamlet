5f8a2635b89aea45e34411b9fc3c649861104601
Andrii <user12341528@gmail.com>
2022-09-22 13:35:09 +0300

    common: * video and channel seeds

--- backend/core/prisma/seed-data/channels.ts
+++ backend/core/prisma/seed-data/channels.ts
@@ -4,19 +4,19 @@ import { users } from './users';
 const channelsNoAuthors: Omit<Channel, 'createdAt' | 'updatedAt' | 'authorId'>[] = [
   {
     'id': '9c6a0001-d379-43e1-9c07-48ab3a5f35bd',
-    'name': 'Alistair Sandoval',
+    'name': 'Binary Studio Academy',
     'description':
-      'Hi! 👋 Welcome to my channel — Im a 24 year old, recent graduate working at a high growth startup! Follow along for insights into the tech industry, all things startup, and what to expect as a recent-grad remote software engineer. I also dabble in sketch-comedy and investing. Thanks for being here!',
-    'contactEmail': 'alistair-sa@ukr.net',
+      'Hi! 👋 Welcome to my channel where you can find out about Binary Studio Academy and it`s participants. Thanks for being here!',
+    'contactEmail': 'alsan@mailsac.com',
     'bannerImage': '',
-    'avatar': 'https://career.comarch.com/files-com/file_45/good_programmer_comarch.jpg',
+    'avatar': 'https://academy.binary-studio.com/static/social-6d2cdde69e2f152e6e9cc7b75d42a1b5.png',
   },
   {
     'id': '916c02ea-ece7-4704-9b15-1376fb7f55f0',
     'name': 'Binary Trip',
     'description':
-      'This year was amazing and I wouldnt have wished for it to have gone any other way. I traveled to a bunch of countries Ive only dreamed of visiting like Mongolia, Kenya, The Philippines, Norway, and many more. Thanks to everyone for sticking around all this time, and I hope you enjoy the videos!',
-    'contactEmail': 'binary-trip@ukr.net',
+      'This year was amazing and I wouldn`t have wished for it to have gone any other way. I traveled to a bunch of countries I`ve only dreamed of visiting like Mongolia, Kenya, The Philippines, Norway, and many more. Thanks to everyone for sticking around all this time, and I hope you enjoy the videos!  ',
+    'contactEmail': 'lufifth@mailsac.com',
     'bannerImage': '',
     'avatar': 'https://static.posters.cz/image/750/road-trip-i107313.jpg',
   },
@@ -24,27 +24,28 @@ const channelsNoAuthors: Omit<Channel, 'createdAt' | 'updatedAt' | 'authorId'>[]
     'id': 'b541916b-d132-4739-a041-a9110b462c40',
     'name': 'Study with us!',
     'description': 'Hello! Subscribe to our channel and start learning programming with us',
-    'contactEmail': '',
+    'contactEmail': 'jayno@mailsac.com',
     'bannerImage': '',
-    'avatar': 'https://www.mooc.org/hubfs/applications-of-computer-programming.jpg',
+    'avatar': 'https://academy.binary-studio.com/static/0-fd1d056d18d5d06e972fa8f518dbd4fb.jpg',
   },
   {
     'id': 'a312816b-d122-4739-a041-a9110b462c40',
-    'name': 'Soft Music',
+    'name': 'Projects and testimonials',
     'description':
-      'Hello everyone who has come to my channel and listen to comfortable and pleasant music. Like and subscribe to the channel to support me!',
-    'contactEmail': 'my-music@ukr.net',
+      'Hello everyone who has come to our channel here you will find interviews with grateful product owners. Like and subscribe to the channel to support us!',
+    'contactEmail': 'haylee@mailsac.com',
     'bannerImage': '',
-    'avatar': 'https://img.freepik.com/free-vector/musical-melody-symbols-bright-blue-splotch_1308-70426.jpg',
+    'avatar':
+      'https://st2.depositphotos.com/2875617/6712/v/600/depositphotos_67127167-stock-illustration-customer-service-and-testimonials.jpg',
   },
   {
     'id': 'c162816b-d122-4739-a041-a9110b462c40',
-    'name': 'BSA',
+    'name': 'CEO',
     'description': 'Find out who we are and what we are doing!',
-    'contactEmail': 'binary-sudio@ukr.net',
+    'contactEmail': 'azrahim@mailsac.com',
     'bannerImage': '',
     'avatar':
-      'https://s.dou.ua/CACHE/images/img/static/companies/dou-logo_cjGiCVW/253030c28d218b03478db06cbefafb44.png',
+      'https://media-exp1.licdn.com/dms/image/C5603AQFaD_35lcWw1A/profile-displayphoto-shrink_800_800/0/1517725784660?e=2147483647&v=beta&t=nyMK5yDIa2LzX-mYO3t1i0xhhD2phkVZIfZRPrOIUUs',
   },
 ];
 
