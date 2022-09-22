f1c464bfcb89b147994aaa8650b02e39309fbb74
foxt451 <svpetrenko123@gmail.com>
2022-09-23 02:18:49 +0300

    frontend-324: - unnesessary liveMaxLatencyDuration that fas-forwarded video whenever it was behind the end

--- frontend/src/components/common/video-player/video-player.tsx
+++ frontend/src/components/common/video-player/video-player.tsx
@@ -107,7 +107,6 @@ const VideoPlayer: FC<VideoPlayerProps> = ({
         // according to docs: "value too low (inferior to ~3 segment durations) is likely to cause playback stalls"
         // if this is a problem, value may be returned to 3, which will make the video start 6 seconds before live point
         liveSyncDuration: 0,
-        liveMaxLatencyDuration: 0.5,
         // ideally, there should be some modifications in the playlist file
         // when the video turns offline, that would differentiate it from live video
         // and let the player start from start automatically
