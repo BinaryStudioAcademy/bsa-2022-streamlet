5b3e3272675debedb7862f9591027c426cec9f85
Barracuda713 <kalitovskyi.bohdan@gmail.com>
2022-09-22 20:04:09 +0300

    backend-265: * fix channel avatar

--- frontend/src/components/sidebar/channel/index.tsx
+++ frontend/src/components/sidebar/channel/index.tsx
@@ -3,7 +3,7 @@ import { FC, BaseSubscriptionResponseDto } from 'common/types/types';
 import { Link } from 'react-router-dom';
 
 import styles from './channel.module.scss';
-import defaultChannelAvatar from 'assets/img/default-channel-avatar.jpg';
+import { UserAvatarOrInitials } from 'components/common/user-avatar-or-initials/user-avatar-or-initials';
 
 interface ChannelProps {
   channelInfo: BaseSubscriptionResponseDto['channel'];
@@ -12,7 +12,11 @@ interface ChannelProps {
 const Channel: FC<ChannelProps> = ({ channelInfo: { id, name, avatar } }) => {
   return (
     <Link to={`${AppRoutes.CHANNEL}/${id}`} className={styles['subscription-item']} title={name}>
-      <img src={avatar ? avatar : defaultChannelAvatar} alt={name} className={styles['channel-avatar']} />
+      <UserAvatarOrInitials
+        className={styles['channel-avatar']}
+        avatar={avatar}
+        userNamingInfo={{ userName: name ?? '' }}
+      />
       <p className={styles['channel-name']}>{name}</p>
     </Link>
   );
